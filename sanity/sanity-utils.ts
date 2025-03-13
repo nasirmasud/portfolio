1ff36983f227964c0f37b43156850a8b90d0a719
Nasir Masud <nasir.masud@ymail.com>
2025-03-13 20:11:37 +0600

    Added utility function to fetch projects from Sanity

---
+++ sanity/sanity-utils.ts
@@ -0,0 +1,20 @@
+import { createClient, groq } from "next-sanity";
+
+export async function getProjects() {
+  const client = createClient({
+    projectId: "39kfav96",
+    dataset: "production",
+    apiVersion: "2023-03-13",
+  });
+  return client.fetch(
+    groq`*[_type == "project"]{
+    _id,
+    _createdAt,
+    name,
+    "slug": slug.current,
+    "image": image.asset->url,
+    url,
+    content
+    }`
+  );
+}
