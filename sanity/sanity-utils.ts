af18cf52da343d3dd78a91f5e1ea0151bc3731d8
Nasir Masud <nasir.masud@ymail.com>
2025-03-15 22:54:06 +0600

    Refactored sanity-utils to use clientConfig and add functions for fetching pages

--- sanity/sanity-utils.ts
+++ sanity/sanity-utils.ts
@@ -1,30 +1,55 @@
+import { Page } from "@/types/Page";
 import { Project } from "@/types/Project";
 import { createClient, groq } from "next-sanity";
-import config from "./config/client-config";
+import clientConfig from "./config/client-config";
 
 export async function getProjects(): Promise<Project[]> {
-  return createClient(config).fetch(
+  return createClient(clientConfig).fetch(
     groq`*[_type == "project"]{
-    _id,
-    _createdAt,
-    name,
-    "slug": slug.current,
-    "image": image.asset->url,
-    url,
+    _id, 
+    _createdAt, 
+    name, 
+    "slug": slug.current, 
+    "image": image.asset->url, 
+    url, 
     content
     }`
   );
 }
 
 export async function getProject(slug: string): Promise<Project> {
-  return createClient(config).fetch(
+  return createClient(clientConfig).fetch(
     groq`*[_type == "project" && slug.current == $slug][0]{
-    _id,
-    _createdAt,
-    name,
-    "slug": slug.current,
-    "image": image.asset->url,
-    url,
+    _id, 
+    _createdAt, 
+    name, 
+    "slug": slug.current, 
+    "image": image.asset->url, 
+    url, 
+    content
+    }`,
+    { slug }
+  );
+}
+
+export async function getPages(): Promise<Page[]> {
+  return createClient(clientConfig).fetch(
+    groq`*[_type == "page"]{
+    _id, 
+    _createdAt, 
+    title, 
+    "slug": slug.current
+    }`
+  );
+}
+
+export async function getPage(slug: string): Promise<Page> {
+  return createClient(clientConfig).fetch(
+    groq`*[_type == "page" && slug.current == $slug][0]{
+    _id, 
+    _createdAt, 
+    title, 
+    "slug": slug.current, 
     content
     }`,
     { slug }
