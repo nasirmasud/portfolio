d6043a863708df40da6e317631013195994d0576
Nasir Masud <nasir.masud@ymail.com>
2025-03-16 12:01:02 +0600

    Fixed site layout width

--- app/(site)/layout.tsx
+++ app/(site)/layout.tsx
@@ -18,7 +18,7 @@ export default async function RootLayout({
 
   return (
     <html lang='en'>
-      <body className='max-w-3xl mx-auto py-10'>
+      <body className='max-w-full mx-auto py-10 px-10'>
         <header className='flex items-center justify-between'>
           <Link
             href='/'
